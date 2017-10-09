5569aae7338a5aabe2d1f56f284572de2eeb80fa
Christian Froehlingsdorf <christian.froehlingsdorf@rewe-digital.com>
2017-10-10 00:09:24 +0200

    basic operations

--- index.js
+++ index.js
@@ -2,33 +2,40 @@
 
 const {Krakn} = require("./lib/Krakn.js");
 
-const krakn = new Krakn({});
+const krakn = new Krakn("kn1", {});
 
 (async () => {
 
     await krakn.init({
-        prefix: "kn1",
+
+        //possible way to extend fields in the node and edge tables
         properties: {
             name: Krakn.TYPES.STRING
         },
         attributes: {
             as: Krakn.TYPES.STRING
         }
-    });
+    }, true);
 
     const nodeHandler = await krakn.getNodeHandler();
 
-    const romeo = await nodeHandler.createNode({name: "Romeo"});
-    const juliet = await nodeHandler.createNode({name: "Juliet"});
+    const romeo = await nodeHandler.createNode("romeo", {someKey: "someValue"}, {name: "Romeo"});
+    const juliet = await nodeHandler.createNode("juliet", {someKey: "someValue"}, {name: "Juliet"});
 
-    const edge1 = await nodeHandler.createEdge(romeo, juliet, {as: "loves"});
-    const edge2 = await nodeHandler.createEdge(juliet, romeo, {as: "hates"});
+    const edge1 = await nodeHandler.createEdge(romeo, juliet, "love", {someKey: "someValue"}, {as: "loves"});
+    const edge2 = await nodeHandler.createEdge(juliet, romeo, "hate", {someKey: "someValue"}, {as: "hates"});
 
-    const rromeo = await nodeHandler.getNodeByPropertyField("name", "Romeo");
-    console.log(rromeo.getProperties());
+    const rromeo = await nodeHandler.getNodeByIdentifier("romeo");
+    await nodeHandler.getNodeByPropertyField("name", "Romeo");
+    console.log(rromeo.getExtend());
 
-    const nodes = (await rromeo.getEdgedNodes()).map(node => node.getProperties().id);
+    const nodes = (await rromeo.getEdgedNodes()).map(node => [
+        node.getKraknID(), node.getIdentifier(), node.getProperties(), node.getExtend()]
+    );
     console.log(nodes);
+
+    const nodex = await rromeo.getEdgedNodes();
+    console.log(nodex[0].getAttributes(), nodex[0].getEdge());
 
     krakn.close();
 })();
