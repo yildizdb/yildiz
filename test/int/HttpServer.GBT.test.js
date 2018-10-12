e3041e779ab0e7ba511b4a52cd49d87dd9922ea8
rjmasikome <rjmasikome@gmail.com>
2018-10-12 16:33:16 +0200

    vb

--- test/int/HttpServer.GBT.test.js
+++ test/int/HttpServer.GBT.test.js
@@ -819,7 +819,7 @@ describe("HttpServer INT", () => {
         assert.equal(body.data.foo, "bar");
     });
 
-    xit("should reset tables based on prefix in bigtable", async() => {
+    it("should reset tables based on prefix in bigtable", async() => {
         const {
             status,
             body
